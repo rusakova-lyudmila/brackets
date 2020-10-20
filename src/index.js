3ff585296746d811fb3d3dee302447f77cb4b5e0
Leonid Rusakov <bengunn87@mail.ru>
2020-10-20 21:55:01 +0500

    feat: complete brackets function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const bracketsArray = bracketsConfig.map(elem => elem.join(''));
+  const strArray = str.split('');
+  const resultArray = [];
+
+  strArray.forEach(item => {
+    resultArray.push(item);
+    
+    if (bracketsArray.includes(resultArray.slice(-2).join(''))) {      
+      resultArray.splice(resultArray.length - 2, 2);      
+    }
+  });
+  
+  return (resultArray.length > 0) ? false : true;
 }
